7a6ca7f157dc0b3ba9c68a4edcf6764dc0afb008
Alexandre Viretti <alexou73@gmail.com>
2021-08-10 17:21:52 +0200

    feat(push): help on merge

--- src/Components/ProductsDetails/ProductsDetails.jsx
+++ src/Components/ProductsDetails/ProductsDetails.jsx
@@ -42,7 +42,8 @@ const ProductDetails = () => {
               <h3>{product.[ 'SELLING NAME' ]}</h3>
               {/* <p>${product.price}</p> */}
               {Object.values(product.prices).map((el) => {
-                      if (parseInt(el.customerNumber, 10) === parseInt(customer)) {
+                console.log((el.userId === parseInt(customer)))
+                      if (el.userId === parseInt(customer)) {
                         return <p>${el.salesPrice}</p>;
                       } else {
                         return(
@@ -57,6 +58,11 @@ const ProductDetails = () => {
                         )
                       }
                     })}
+                    {/* {Object.values(hit.prices).map((el) => {
+                    if (el.userId === parseInt(customer)) {
+                      return <p>${el.salesPrice}</p>;
+                    }
+                  })} */}
               <p>{product.description}</p>
             </div>
             <img src={pdp} alt="" />
